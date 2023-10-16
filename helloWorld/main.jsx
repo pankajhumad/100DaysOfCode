6ab615efaf36c837d3d500f20b08b04231ef72be
Pankaj Humad <pankajhumad@hotmail.com>
2023-10-16 06:34:58 -0400

    Hello World

--- helloWorld/main.jsx
+++ helloWorld/main.jsx
@@ -1,5 +1,6 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 
-export default function HelloWorld(){
-  return <h1>Hello World</h1>;
-}
+// Render your React component instead
+const domNode = document.getElementById('test');
+const root = createRoot(domNode);
+root.render(<h1>Hello, world</h1>);
