d2a485b1d4e5b27671ac484a2702788e6a83cfa0
Pankaj Humad <pankajhumad@hotmail.com>
2023-10-17 08:27:10 -0400

    Hello World

--- helloWorld/main.jsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-// Render your React component instead
-const domNode = document.getElementById('test');
-const root = createRoot(domNode);
-root.render(<h1>Hello, world</h1>);
